6cc1040d2128e99a5a9846aea59c82b4b3c5ecca
Marcin Wiącek <marcin@mwiacek.com>
2024-04-01 20:38:42 +0200

    Updates

--- project3_in_progress/cpu.v
+++ project3_in_progress/cpu.v
@@ -1,27 +1,38 @@
+`define OPCODE_READRAM8 1
+`define OPCODE_JUMPMINUS 2
+
 module cpu(input rst, input ram_clk);
-  reg stage1_clk;
+  reg stage12_clk;
   reg [15:0]pc;
-  wire stage1_ready;
-  stage1 stage1(.pc(pc), .stage1_clk(stage1_clk), .rst(rst),.ram_clk(ram_clk),
-  	.stage1_ready(stage1_ready));
+  wire stage12_ready;
+  wire [15:0]stage3_read_address;
+  wire stage3_read;
+  
+  stage12 stage12(.pc(pc), .stage12_clk(stage12_clk), .rst(rst),.ram_clk(ram_clk),
+  	.stage12_ready(stage12_ready),.stage3_read_address(stage3_read_address), .stage3_read(stage3_read));
   
   always @(rst) begin
     	$display($time,"reset");
     	pc=0;
-    	stage1_clk=1;
+    	stage12_clk=1;
   end
-  always @(posedge stage1_ready) begin
-	$display($time,"posedge stage1ready");
-   	pc=pc+4;
-       	stage1_clk=0;
+  always @(posedge stage12_ready) begin
+	$display($time,"posedge stage12ready");
+	//if (instruction[0]=OPCODE_JUMPMINUS) begin
+	//	pc-=instruction[1]*4;
+//	end else begin
+	   	pc=pc+4;
+//	end
+       	stage12_clk=0;
   end
-  always @(negedge stage1_clk) begin
-   	$display($time,"negedge stage1ready");
-    	stage1_clk=1;
+  always @(negedge stage12_clk) begin
+   	$display($time,"negedge stage12ready");
+    	stage12_clk=1;
   end
 endmodule
 
-module stage1(input [15:0]pc, input stage1_clk, input rst,input ram_clk, output reg stage1_ready);
+module stage12(input [15:0]pc, input stage12_clk, input rst,input ram_clk,
+  output reg stage12_ready, output reg [15:0]stage3_read_address, output reg stage3_read);
   reg ram_write_enable;
   reg [15:0]ram_address;
   reg [7:0]ram_data_in;
@@ -31,8 +42,8 @@ module stage1(input [15:0]pc, input stage1_clk, input rst,input ram_clk, output 
 
   reg [7:0]instruction[0:3];
  
-  always @ (stage1_clk) begin
-	stage1_ready <= 0;
+  always @ (stage12_clk) begin
+	stage12_ready <= 0;
     	ram_write_enable = 0; 
 	$display($time," executing pc ",pc);
 	ram_address = pc;
@@ -52,8 +63,16 @@ module stage1(input [15:0]pc, input stage1_clk, input rst,input ram_clk, output 
 	@(posedge ram_clk)
 	instruction[3] = ram_data_out;
 	$display($time," ",instruction[0], " ", instruction[1]," ",
-		instruction[2]," ",instruction[3]);    
-	stage1_ready<=1;
+		instruction[2]," ",instruction[3]);
+	stage3_read<=0;	
+	if (instruction[0]==`OPCODE_READRAM8) begin
+		$display($time," READRAM8");
+		stage3_read<=1;
+		stage3_read_address=instruction[1];
+	end else if (instruction[0]==`OPCODE_JUMPMINUS) begin
+		$display($time," JUMPMINUS");
+	end
+	stage12_ready<=1;
   end
 endmodule
 
