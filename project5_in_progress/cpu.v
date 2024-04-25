972e68e1e30fb24bc31ffdfe799b4cdb4824601b
Marcin Wiącek <marcin@mwiacek.com>
2024-04-25 20:02:21 +0200

    Updates

--- project5_in_progress/cpu.v
+++ project5_in_progress/cpu.v
@@ -65,6 +65,12 @@ module stage1 (
   reg [7:0] loop_counter = 0;
   reg [7:0] loop_counter_max = 0;
   reg [5:0] loop_regnum;
+  reg [7:0] loop_value;
+  reg [1:0] loop_type;
+
+  `define LOOP_TILL_VALUE 1
+  `define LOOP_TILL_NON_VALUE 2
+  `define LOOP_FOR 3
 
   `define STAGE_READ_PC1_REQUEST 0
   `define STAGE_READ_PC1_RESPONSE 1
@@ -75,13 +81,20 @@ module stage1 (
   `define STAGE_SAVE_REG2RAM 6
 
   `define OPCODE_JMP 1     //x, 16 bit address
-  `define OPCODE_RAM2REG 2 //register num, 16 bit source addr
-  `define OPCODE_REG2RAM 3 //register num, 16 bit source addr
-  `define OPCODE_NUM2REG 4 //register num, 16 bit value
-  `define OPCODE_REGPLUSNUM 5 //register num, 16 bit value
-  `define OPCODE_REGMINUSNUM 6 //register num, 16 bit value
-  `define OPCODE_LOOPTILLZERO 7   //register num, x, how many (8 bit value)
-  `define OPCODE_LOOPTILLNONZERO 8   //register num, x, how many (8 bit value)
+  `define OPCODE_RAM2REG 2 //register num, 16 bit source addr //ram -> reg
+  `define OPCODE_REG2RAM 3 //register num, 16 bit source addr //reg -> ram
+  `define OPCODE_NUM2REG 4 //register num, 16 bit value //value -> reg
+  `define OPCODE_REG_PLUS 5 //register num, 16 bit value // reg += value
+  `define OPCODE_REG_MINUS 6 //register num, 16 bit value  //reg -= value
+  `define OPCODE_REG_MUL 7 //register num, 16 bit value // reg *= value
+  `define OPCODE_REG_DIV 8 //register num, 16 bit value  //reg /= value
+  `define OPCODE_TILL_VALUE 9   //register num, value, how many instructions (8 bit value) // do..while
+  `define OPCODE_TILL_NON_VALUE 10   //register num, value, how many instructions (8 bit value) //do..while
+  `define OPCODE_LOOP 11   //x, x, how many instructions (8 bit value) //for...
+  `define OPCODE_PROC 12 //new process
+  `define OPCODE_REG_INT 14
+  `define OPCODE_INT 15
+  `define OPCODE_INT_RET 16
 
   always @(posedge rst) begin
     $display($time, "rst");
@@ -98,8 +111,8 @@ module stage1 (
         inst_address_num <= inst_address_num_cache[loop_counter_max];
         loop_counter_max <= loop_counter_max + 1;
         address_pc <= address_pc + 2;
-        $display($time, (address_pc), "=", inst_op, inst_regnum,
-                 inst_address_num / 256, inst_address_num % 256, " (cache)");
+        $display($time, (address_pc), "=", inst_op, inst_regnum, inst_address_num / 256,
+                 inst_address_num % 256, " (cache)");
         stage <= `STAGE_DECODE;
       end else begin
         addrb <= address_pc;
@@ -128,29 +141,51 @@ module stage1 (
         $display($time, " opcode = num2reg value ", inst_address_num, " to reg ", inst_regnum);
         registers[inst_regnum] <= inst_address_num;
         stage <= `STAGE_READ_PC1_REQUEST;
-      end else if (inst_op == `OPCODE_REGPLUSNUM) begin
+      end else if (inst_op == `OPCODE_REG_PLUS) begin
         $display($time, " opcode = regplusnum value ", inst_address_num, " to reg ", inst_regnum);
         registers[inst_regnum] <= registers[inst_regnum] + inst_address_num;
         stage <= `STAGE_READ_PC1_REQUEST;
-      end else if (inst_op == `OPCODE_REGMINUSNUM) begin
+      end else if (inst_op == `OPCODE_REG_MINUS) begin
         $display($time, " opcode = regminusnum value ", inst_address_num, " to reg ", inst_regnum);
         registers[inst_regnum] <= registers[inst_regnum] - inst_address_num;
         stage <= `STAGE_READ_PC1_REQUEST;
-      end else if (inst_op == `OPCODE_LOOPTILLZERO) begin
-        $display($time, " opcode = loopeqzero ", inst_address_num % 256, " instruction, reg ",
+      end else if (inst_op == `OPCODE_TILL_VALUE) begin
+        $display($time, " opcode = looptillvalue ", inst_address_num % 256, " instruction, reg ",
                  inst_regnum);
         loop_counter <= 0;
-        loop_counter_max <= inst_address_num % 256;
         loop_regnum <= inst_regnum;
+        loop_value <= inst_address_num / 256;
+        loop_counter_max <= inst_address_num % 256;
+        loop_type <= `LOOP_TILL_VALUE;
+        stage <= `STAGE_READ_PC1_REQUEST;
+      end else if (inst_op == `OPCODE_TILL_NON_VALUE) begin
+        $display($time, " opcode = looptillnonvalue ", inst_address_num % 256,
+                 " instruction, reg ", inst_regnum);
+        loop_counter <= 0;
+        loop_regnum <= inst_regnum;
+        loop_value <= inst_address_num / 256;
+        loop_counter_max <= inst_address_num % 256;
+        loop_type <= `LOOP_TILL_NON_VALUE;
+        stage <= `STAGE_READ_PC1_REQUEST;
+      end else if (inst_op == `OPCODE_LOOP) begin
+        $display($time, " opcode = looploop ", inst_address_num % 256, " instruction, reg ",
+                 inst_regnum);
+        loop_counter <= 0;
+        loop_value <= inst_address_num / 256;
+        loop_counter_max <= inst_address_num % 256;
+        loop_type <= `LOOP_FOR;
         stage <= `STAGE_READ_PC1_REQUEST;
       end else begin
         $display($time, " opcode = ", inst_op);
         stage <= `STAGE_READ_PC1_REQUEST;
       end
       if (loop_counter_max != 0 && loop_counter_max == loop_counter) begin
-        if (registers[loop_regnum] != 0) begin
+        if ((loop_type == `LOOP_TILL_VALUE && registers[loop_regnum] != loop_value) ||
+        (loop_type == `LOOP_TILL_NON_VALUE && registers[loop_regnum] == loop_value) ||
+        (loop_type == `LOOP_FOR && loop_value>0)) begin
           address_pc <= address_pc - loop_counter * 2;
           loop_counter_max <= 0;
+          if (loop_type == `LOOP_FOR) loop_value <= loop_value - 1;
         end else begin
           loop_counter <= 0;
           loop_counter_max <= 0;
