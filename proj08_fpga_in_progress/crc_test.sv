8ec00e32a1cbc4c519b2c4a7bbacc8ee19aaf1da
Marcin Wiącek <marcin@mwiacek.com>
2024-10-20 22:54:05 +0200

    Updates

--- proj08_fpga_in_progress/crc_test.sv
+++ proj08_fpga_in_progress/crc_test.sv
@@ -18,14 +18,24 @@ initial begin
     for (i=0;i<40;i=i+1) begin
    //Generator polynomial x^7 + x^3 + 1
                #1
-               temp = cmd[46];
+               temp = cmd[40];
+               cmd[40] = cmd[41];
+               cmd[41] = cmd[42];
+               cmd[42] = cmd[43];
+               cmd[43] = cmd[44]^(cmd[i] ^ temp);
+               cmd[44] = cmd[45];
+               cmd[45] = cmd[46];
+               cmd[46] = cmd[i] ^ temp;
+               
+               
+               /*temp = cmd[46];
                cmd[46] = cmd[45];              
                cmd[45] = cmd[44];              
                cmd[44] = cmd[43];
                cmd[43] = cmd[42]^( cmd[i] ^ temp);
                cmd[42] = cmd[41];
                cmd[41] = cmd[40];
-               cmd[40] = cmd[i] ^ temp;
+               cmd[40] = cmd[i] ^ temp;*/
     end
     $display(cmd[0:7]);
     $display(cmd[40:46]);
