c00e8f45940695068b3d9768d9b65469de4b1792
Marcin Wiącek <marcin@mwiacek.com>
2024-10-26 22:41:32 +0200

    Updates

--- proj08_fpga_in_progress/x_out_of_order.sv
+++ proj08_fpga_in_progress/x_out_of_order.sv
@@ -40,23 +40,23 @@ module x_out_of_order (
     output reg x
 );
 
-//-------------------------------------------------------alu---------------------------------
+  //-------------------------------------------------------alu---------------------------------
 
-   reg alu_inp;
-   wire alu_ready;
-     reg [5:0] alu_op;
-   reg [15:0] alu_arg1,alu_arg2;
-    wire [15:0] value;
+  reg alu_inp;
+  wire alu_ready;
+  reg [5:0] alu_op;
+  reg [15:0] alu_arg1, alu_arg2;
+  wire [15:0] value;
 
-alu alu (
-    .clk(clk),
-    .inp(alu_inp),
-    .ready(alu_ready),
-    .op(alu_op),
-    .arg1(alu_arg1),
-    .arg2(alu_arg2),
-    .value(alu_value)
-);
+  alu alu (
+      .clk(clk),
+      .inp(alu_inp),
+      .ready(alu_ready),
+      .op(alu_op),
+      .arg1(alu_arg1),
+      .arg2(alu_arg2),
+      .value(alu_value)
+  );
 
   //--------------------------------------------------------- mmu ----------------------------
 
@@ -393,7 +393,8 @@ module alu (
     input inp,
     output bit ready = 1,
     input reg [5:0] op,
-    input reg [15:0] arg1,arg2,
+    input reg [15:0] arg1,
+    arg2,
     output reg [15:0] value
 );
 
@@ -401,10 +402,10 @@ module alu (
     ready <= inp;
     if (inp) begin
       case (op)
-      ALU_ADD: value = arg1+arg2;
-      ALU_DEC: value = arg1-arg2;
-      endcase      
-      $display($time, " alu ", arg1, " ",arg2);
+        ALU_ADD: value = arg1 + arg2;
+        ALU_DEC: value = arg1 - arg2;
+      endcase
+      $display($time, " alu ", arg1, " ", arg2);
     end
   end
 endmodule
