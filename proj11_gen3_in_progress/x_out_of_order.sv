d12c1e4100e813e948302c5f029f4ce8406ad6cd
Marcin Wiącek <marcin@mwiacek.com>
2024-11-09 21:56:08 +0100

    Updates

--- proj11_gen3_in_progress/x_out_of_order.sv
+++ proj11_gen3_in_progress/x_out_of_order.sv
@@ -140,13 +140,13 @@ module x_out_of_order (
       .read_value2  (read_value2)
   );
 
-  /*parameter SAVERAM_QUEUE_LEN = 50;
+  parameter SAVERAM_QUEUE_LEN = 10;
 
   typedef struct {reg [15:0] addr,value;} saveram;
 
   saveram saveram_q[0:SAVERAM_QUEUE_LEN];
   reg [7:0] saveram_q_new_pos;
-*/
+
   //--------------------------------------------------------------------process------------------
 
   reg [15:0] process_hardware_address = 0;
