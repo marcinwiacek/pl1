605ab85f2e8251440322d51f8a5be6f80df2081e
Marcin Wiącek <marcin@mwiacek.com>
2024-11-17 18:11:48 +0100

    Updates

--- proj11_gen3_in_progress/x_out_of_order.sv
+++ proj11_gen3_in_progress/x_out_of_order.sv
@@ -297,7 +297,7 @@ module x_out_of_order (
                 end else begin
                   case (`INSTRUCTION_STATE)
                     OPCODE_REG2RAM: begin
-                        saveram_q_addr[(saveram_q_new_pos +i-`REG_START_NUM)%32]<= `INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC+`REG_START_NUM-i;
+                    saveram_q_value[(saveram_q_new_pos +i-`REG_START_NUM)%32]<= `REG_VALUE(i);
                     end
                     OPCODE_REG_PLUS:
                     registers[i] <= `REG_VALUE(i) + `INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC;
@@ -314,15 +314,13 @@ module x_out_of_order (
           end
         end
         if (!fetch_stall_exists && `INSTRUCTION_STATE == OPCODE_REG2RAM) begin
-           xx = saveram_q_new_pos % 32;
             for (i = 0; i < 32; i = i + 1) begin
                if (i >= `REG_START_NUM && i <= `REG_END_NUM) begin
                       $display($time, pc_logical, " save ram initiate ", (
                          `INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC+`REG_START_NUM-i), " ",`REG_VALUE(i),
-                         " position ",(saveram_q_new_pos+i-`REG_START_NUM)%32);  //DEBUG info                                                          
-                     saveram_q_value[xx]<=i;
-                     saveram_q_mmu_done[xx]<=0;
-                     xx = (xx+1)%32;
+                         " position ",(saveram_q_new_pos+i-`REG_START_NUM)%32);  //DEBUG info
+                         saveram_q_addr[(saveram_q_new_pos +i-`REG_START_NUM)%32]<= `INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC+`REG_START_NUM-i;                                                          
+                     saveram_q_mmu_done[(saveram_q_new_pos +i-`REG_START_NUM)%32]<=0;
                end
             end
             saveram_q_new_pos <= (saveram_q_new_pos+`REG_END_NUM-`REG_START_NUM)%32;
@@ -341,7 +339,6 @@ module x_out_of_order (
           saveram_q_num<=50;
       write_enabled<=0;    
       for (i = 0; i < 32; i = i + 1) begin
-      saveram_q_value[i]<=registers[i];
         if (saveram_q_mmu_done[i] && i!=saveram_q_num) begin
           write_address <= saveram_q_addr[i];
           write_value <= saveram_q_value[i];      
