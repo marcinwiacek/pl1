cce07491c816395e380a08c120feea2dbcfa7572
Marcin Wiącek <marcin@mwiacek.com>
2024-11-17 20:02:22 +0100

    Updates

--- proj11_gen3_in_progress/x_out_of_order.sv
+++ proj11_gen3_in_progress/x_out_of_order.sv
@@ -191,7 +191,7 @@ module x_out_of_order (
   
   reg [32:0] xx,xy;
 
-  integer i;
+  integer i,j;
 
   //always @(posedge clk) begin
     
@@ -296,14 +296,12 @@ module x_out_of_order (
                   end
                 end else begin
                   case (`INSTRUCTION_STATE)
-                   OPCODE_REG2RAM: begin
-                   // saveram_q_value[(saveram_q_new_pos +i-`REG_START_NUM)%32]<= `REG_VALUE(i);
-                                         $display($time, pc_logical, " save ram initiate ", (
-                         `INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC+`REG_START_NUM-i), " ",`REG_VALUE(i),
-                         " position ",(saveram_q_new_pos+i-`REG_START_NUM)%32);  //DEBUG info
-                         saveram_q_addr= {`INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC+`REG_START_NUM-i,saveram_q_addr[1:32]};                                                          
-                     saveram_q_mmu_done={0,saveram_q_mmu_done[1:32]};
-                      saveram_q_value= {`REG_VALUE(i),saveram_q_value[1:32]};      
+                    OPCODE_REG2RAM: begin
+                      for (j=0;j<32;j=j+1) begin
+                         saveram_q_addr[j]<= `INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC+`REG_START_NUM-i;
+                         saveram_q_value[j]<=registers[i];
+                         saveram_q_mmu_done[j]<=0;
+                       end
                     end
                     OPCODE_REG_PLUS:
                     registers[i] <= `REG_VALUE(i) + `INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC;
@@ -320,13 +318,15 @@ module x_out_of_order (
           end
         end
         if (!fetch_stall_exists && `INSTRUCTION_STATE == OPCODE_REG2RAM) begin
-         /*   for (i = 0; i < 32; i = i + 1) begin
+          /* xx = saveram_q_new_pos % 32;
+            for (i = 0; i < 32; i = i + 1) begin
                if (i >= `REG_START_NUM && i <= `REG_END_NUM) begin
                       $display($time, pc_logical, " save ram initiate ", (
                          `INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC+`REG_START_NUM-i), " ",`REG_VALUE(i),
-                         " position ",(saveram_q_new_pos+i-`REG_START_NUM)%32);  //DEBUG info
-                         saveram_q_addr[(saveram_q_new_pos +i-`REG_START_NUM)%32]<= `INSTRUCTION_START_RAM_ADDRESS_OR_NUMERIC+`REG_START_NUM-i;                                                          
-                     saveram_q_mmu_done[(saveram_q_new_pos +i-`REG_START_NUM)%32]<=0;
+                         " position ",(saveram_q_new_pos+i-`REG_START_NUM)%32);  //DEBUG info                                                          
+                     saveram_q_value[xx]<=i;
+                     saveram_q_mmu_done[xx]<=0;
+                     xx = (xx+1)%32;
                end
             end*/
             saveram_q_new_pos <= (saveram_q_new_pos+`REG_END_NUM-`REG_START_NUM)%32;
@@ -345,6 +345,7 @@ module x_out_of_order (
           saveram_q_num<=50;
       write_enabled<=0;    
       for (i = 0; i < 32; i = i + 1) begin
+      saveram_q_value[i]<=registers[i];
         if (saveram_q_mmu_done[i] && i!=saveram_q_num) begin
           write_address <= saveram_q_addr[i];
           write_value <= saveram_q_value[i];      
