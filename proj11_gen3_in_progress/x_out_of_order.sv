4789389e3f157bd76c8ba5b4ccca256c74dd7743
Marcin Wiącek <marcin@mwiacek.com>
2025-02-03 21:19:19 +0100

    Updates

--- proj11_gen3_in_progress/x_out_of_order.sv
+++ proj11_gen3_in_progress/x_out_of_order.sv
@@ -207,21 +207,22 @@ module x_out_of_order (
   reg [15:0] registerr[0:1];
   reg [15:0] read_valueee, read_valueee2;
   reg [6:0] registers_save_countereeee, registers_save_countereeee2;
-  reg [6:0] p, q;
+  reg [6:0] p, q,r;
 
   always @(posedge clk) begin
     registerr[0] = 0;
     if (register[0] != RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32) begin
-      registers_save_countereeee = 0;
+      for (r = 0; r < 32; r = r + 1) begin
+    
       for (j = 0; j < REGISTER_NUM; j = j + 1) begin
-        if (read_address == registers_save_address[j]) begin
           p = registers_save_save_counter[j];
-//          if (p >registers_save_countereeee) begin //&& p<save_counter_for_read) begin
-            read_valueee =p >registers_save_countereeee?registers_save[j]:read_valueee;
-            registers_save_countereeee= p >registers_save_countereeee?p:registers_save_countereeee;
-  //        end
+        if (read_address == registers_save_address[j] && p == r && r<save_counter_for_read) begin
+            read_valueee =registers_save[j];
+            registers_save_countereeee= p;
           registerr[0] = 1;
         end
+      end
+      
       end
     end
   end
