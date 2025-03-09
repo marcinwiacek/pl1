18a8a35c3d51cab6ef5781801e17fd99154d0e1a
Marcin Wiącek <marcin@mwiacek.com>
2025-03-09 21:35:40 +0100

    Updates

--- proj11_gen3_in_progress/x_out_of_order2.sv
+++ proj11_gen3_in_progress/x_out_of_order2.sv
@@ -188,7 +188,24 @@ module x_out_of_order2 (
   reg rst = 1;
   reg [7:0] instr_num = 0;  // how many done
 
-  integer i;
+  integer i,j;
+
+always @(posedge clk) begin
+ write_enabled <= saveram_q_num != RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32;
+      if (saveram_q_num != RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32) begin
+       
+        write_address <= registers_save_address2[saveram_q_num];
+        write_value <= registers_save[saveram_q_num];
+        saveram_q_num <= RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32;
+     
+      end else begin
+        for (j = 0; j < REGISTER_NUM; j = j + 1) begin
+          if (registers_save_ready[j]) begin  // && registers_save_save_counter[i] == 0) begin
+            saveram_q_num <= j;
+          end
+        end
+      end
+end
 
   always @(posedge clk) begin
     if (rst) begin
@@ -222,25 +239,12 @@ module x_out_of_order2 (
       // $display("");
       // $display("save ram ", saveram_q_num, " save counter ", save_counter);
       //save ram              
-      write_enabled <= saveram_q_num != RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32;
-      if (saveram_q_num != RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32) begin
+     if (saveram_q_num != RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32) begin
         register_save_lock[saveram_q_num] <= 0;
         registers_save_ready[saveram_q_num] <= 0;
         registers_save_mmu_done[saveram_q_num] <= 0;
-        write_address <= registers_save_address2[saveram_q_num];
-        write_value <= registers_save[saveram_q_num];
-        saveram_q_num <= RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32;
-        for (i = 0; i < REGISTER_NUM; i = i + 1) begin
-          //          registers_save_save_counter[i]<=registers_save_save_counter[i]>0?registers_save_save_counter[i]-1:0;
-          //        registers_save_counter[i]<=registers_save_counter[i]>0?registers_save_counter[i]-1:0;
-        end
+     
         save_counter = save_counter > 0 ? save_counter - 1 : 0;
-      end else begin
-        for (i = 0; i < REGISTER_NUM; i = i + 1) begin
-          if (registers_save_ready[i]) begin  // && registers_save_save_counter[i] == 0) begin
-            saveram_q_num <= i;
-          end
-        end
       end
       if (register[0] != RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32) begin
         $display($sformatf("%02d", $time), pc_logical, " no fetch register ", register[0],
