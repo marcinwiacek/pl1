e808806bef44f76662807decfa8354b531595bb4
Marcin Wiącek <marcin@mwiacek.com>
2025-03-22 22:56:39 +0100

    Timing OK

--- proj11_gen3_in_progress/x_out_of_order2.sv
+++ proj11_gen3_in_progress/x_out_of_order2.sv
@@ -174,7 +174,8 @@ module x_out_of_order2 (
         // verilog_format:on
       },  //Read from RAM?
       registers_save_ready[0:REGISTER_NUM-1],
-      registers_save_mmu_done[0:REGISTER_NUM-1];
+      registers_save_mmu_done[0:REGISTER_NUM-1],
+      registers_read_stall[0:REGISTER_NUM-1];
 
   //----------------------------------------------------------------other---------------------------
 
@@ -200,14 +201,21 @@ module x_out_of_order2 (
 
   always @(posedge clk) begin
     read_stall_exists = 0;
+   // registers_read_stall = '{default: 0};
     if (reg_instruction_state == OPCODE_REG2RAM || reg_instruction_state == OPCODE_RAM2REG) begin
       for (z = 0; z < REGISTER_NUM; z = z + 1) begin
         if (registers_save_address[z]>=reg_start_ram_address_or_numeric && 
                     registers_save_address[z]<=reg_start_ram_address_or_numeric + reg_register_len) begin
           read_stall_exists = 1;  //do save before
+          
           $display($sformatf("%02d", $time), pc_logical, " read stall exists");
         end
       end
+    end
+    if (read_stall_exists) begin
+      for (z = 0; z < REGISTER_NUM; z = z + 1) begin
+        registers_read_stall[z]=reg_do_op[z];
+      end      
     end
   end
 
@@ -239,7 +247,7 @@ module x_out_of_order2 (
                          registers_save_address[i]));
       end
       $display("");
-      if (((save_stall_exists && read_stall_exists) || !read_stall_exists)) begin
+      //if (((save_stall_exists && read_stall_exists) || !read_stall_exists)) begin
         if (register[0] != RANDOM_SELECTED_EMPTY_VALUE_HIGHER_THAN_32) begin
           $display($sformatf("%02d", $time), " read first slot register ", register[0],
                    " with address ",  //DEBUG info
@@ -254,9 +262,9 @@ module x_out_of_order2 (
           registers_value[register[1]] = read_value2;
           registers_init[register[1]]  = 1;
         end
-      end
+     // end
       for (i = 0; i < REGISTER_NUM; i = i + 1) begin
-        if (!registers_init[i]) begin
+        if (!registers_init[i] && !registers_read_stall[i]) begin
           if (registers_src_mmu_done[i]) begin
             //  $display($sformatf("%02d", $time), pc_logical, " reg to read, with mmu ", i);
             if (i % 2 == 0) begin
