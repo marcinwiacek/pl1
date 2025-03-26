8c8ccad17f131e0df1a55d5a165c458a7db28fcd
Marcin Wiącek <marcin@mwiacek.com>
2025-03-26 23:49:16 +0100

    Updates

--- proj11_gen3_in_progress/x_out_of_order2.sv
+++ proj11_gen3_in_progress/x_out_of_order2.sv
@@ -186,43 +186,40 @@ module x_out_of_order2 (
 
   integer i, j, z;
 
-  /*always @(posedge clk) begin
-    save_stall_exists <= 0;
-    if (reg_instruction_state == OPCODE_REG2RAM) begin
+always @(posedge clk) begin
+    read_stall1_exists <= 0;
+   
+    //registers_read_stall <= '{default: 0};
+    if (reg_instruction_state == OPCODE_RAM2REG) begin
       for (j = 0; j < REGISTER_NUM; j = j + 1) begin
-        if (!registers_init[j]) begin
+        
+         
           if (registers_src_address[j] >= reg_start_ram_address_or_numeric) begin
             if (registers_src_address[j]<=reg_start_ram_address_or_numeric + reg_register_len) begin
-              save_stall_exists <= 1;  //do reads before save
-              $display($sformatf("%02d", $time), pc_logical, " save stall exists");
+              read_stall1_exists <= 1;  //do reads before save
+              $display($sformatf("%02d", $time), pc_logical, " read1 stall exists");
             end
-          end
+          
+        
         end
       end
     end
   end
-*/
 
   always @(posedge clk) begin
-    read_stall1_exists <= 0;
+   
     read_stall2_exists <= 0;
     //registers_read_stall <= '{default: 0};
     if (reg_instruction_state == OPCODE_RAM2REG) begin
       for (z = 0; z < REGISTER_NUM; z = z + 1) begin
         if (registers_save_address[z] >= reg_start_ram_address_or_numeric) begin
           if (registers_save_address[z]<=reg_start_ram_address_or_numeric + reg_register_len) begin
-            read_stall1_exists <= 1;  //do save before
-            $display($sformatf("%02d", $time), pc_logical, " read1 stall exists");
+            read_stall2_exists <= 1;  //do save before
+            $display($sformatf("%02d", $time), pc_logical, " read2 stall exists");
             //  registers_read_stall[z] <= 1;
           end
-          if (!registers_init[z]) begin
-          if (registers_src_address[z] >= reg_start_ram_address_or_numeric) begin
-            if (registers_src_address[z]<=reg_start_ram_address_or_numeric + reg_register_len) begin
-              read_stall2_exists <= 1;  //do reads before save
-              $display($sformatf("%02d", $time), pc_logical, " read2 stall exists");
-            end
-          end
-        end
+          
+       
         end
       end
     end
